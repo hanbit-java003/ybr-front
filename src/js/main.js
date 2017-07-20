8b82ce639efd208ff208123eb61afcad8a2c69fd
s <chunwooSv@gmail.com>
2017-07-20 16:20:46 +0900

    수정(2017.07.20 17:55)

--- src/js/main.js
+++ src/js/main.js
@@ -12,3 +12,34 @@ $('.say-hello').on('click', function() {
 $('.goto-sub').on('click', function() {
     location.href = 'sub.html';
 });
+
+var topOne = require('./model/top-one');
+
+function initTopOne() {
+    $('.m-tops-li').empty();
+
+    var topOneTemplate =  require('../template/top-one.hbs');
+
+    for (var i=0; i<topOne.length; i++) {
+        var html = topOneTemplate(topOne[i]);
+
+        $('.m-tops-li').append(html);
+    }
+}
+
+var topLank = require('./model/top-lank');
+
+function initTopLank() {
+    $('.m-top-gen-li').empty();
+
+    var topLankTemplate = require('../template/top-lank.hbs');
+
+    for (var i=0; i<topLank.length; i++) {
+        var html = topLankTemplate(topLank[i]);
+
+        $('.m-top-gen-li').append(html);
+    }
+}
+
+initTopOne();
+initTopLank();
--- src/js/top.js
+++ src/js/top.js
@@ -3,8 +3,3 @@ require('../less/theres.less');
 
 var common = require('./common');
 
-var topOneModel = require('./model/top-one');
-var topOneTemplate = require('../template/top-one.hbs');
-var topOneHtml = topOneTemplate(topOneModel);
-
-$('.m-top-one').html(topOneHtml);
